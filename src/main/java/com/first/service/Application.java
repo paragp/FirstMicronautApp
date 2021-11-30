03ac91a413516c28f3e6cb2518a90fb8c2d42911
Parag <paragp>
2021-11-30 19:48:53 +0000

    Initial commit

---
+++ src/main/java/com/first/service/Application.java
@@ -0,0 +1,10 @@
+package com.first.service;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
